e4b02fc523300bf066dfc4ba434732b8a90c4463
assr1998 <78389144+assr1998@users.noreply.github.com>
2021-06-22 12:04:19 -0300

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+
+const databaseOptions = {
+    database: 'delilah_resto_db',
+    username: 'root',
+    password: null,
+    dialect: 'mysql'
+}
+
+const signature = 'Acamica2021'
+
+module.exports = {
+    databaseOptions,
+    signature
+}
